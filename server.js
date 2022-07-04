727d37e7fc173b852a0e13490b173eced846f234
apozzi <axelpozzi@gmail.com>
2022-07-04 15:24:53 -0500

    updating database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const profile = require('./controllers/profile');
 const db=knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-amorphous-91185',
       port : 5432,
       user : 'postgres',
       password : 'test',
